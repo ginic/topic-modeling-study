e0d03a0c64ad276b9ce188011667548745b0327c
Virginia Partridge <vcpartridge@gmail.com>
2021-01-18 22:28:05 -0500

    Init bib and preprocessing script

---
+++ topic_modeling/preprocessing.py
@@ -0,0 +1,10 @@
+# -*- coding: uft-8 -*-
+
+"""Preprocessing (primarily) Russian text for topic modeling."""
+
+__author__= "Virginia Partridge"
+
+# TODO Break up Gutenberg texts into Mallet TSV format, stripping tabs out of text
+# [doc ID] [tab] [label] [tab] [text...]
+
+
