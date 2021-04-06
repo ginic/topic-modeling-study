008de38f2bc7a9dca11426ef0f9aed1348f95b36
Virginia Partridge <vcpartridge@gmail.com>
2021-04-06 15:06:45 -0400

    Mallet parser added

---
+++ topic_modeling/mallet_parser.py
@@ -0,0 +1,21 @@
+"""Functions for dealing with Mallet's diagnostics and state files
+"""
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+# Diagnostics XML constants
+TOPIC="topic"
+TOPIC_ID="id"
+
+
+def diagnostics_xml_to_dataframe(xml_path):
+    """Returns a pandas dataframe with the
+    :param xml_path: Path to a Mallet diagnostics file
+    """
+    model_root = ET.parse(xml_path).getroot()
+    topic_metrics = list()
+    for t in model_root.findall(TOPIC):
+        topic_metrics.append(t.attrib)
+
+    return pd.Dataframe.from_records(topic_metrics, index=TOPIC_ID)
+
