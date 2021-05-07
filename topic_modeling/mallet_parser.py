c266f8cab0162b6218f5a1c6b0ad0bb39fe2a9cf
Virginia Partridge <vcpartridge@gmail.com>
2021-05-06 21:23:47 -0400

    Plots for entropy metrics

--- topic_modeling/mallet_parser.py
+++ topic_modeling/mallet_parser.py
@@ -79,7 +79,7 @@ def append_topic_morphological_analysis_to_file(topic_id, tsv_file, weighted_mor
         weighted_proportion = v / total_counts
         unweighted_count = unweighted_morph_analysis_counts[k]
         unweighted_proportion =  unweighted_count / total_counts
-        results.append([topic_id, k, v, unweighted_count, weighted_proportion])
+        results.append([topic_id, k, v, weighted_proportion, unweighted_count, unweighted_proportion])
 
     sorted_results = sorted(results, key=lambda x: x[3], reverse=True)
 
@@ -231,7 +231,7 @@ def morphological_entropy_all_topics(in_state_file, slot_analysis_file=None, lem
         tmp_list.extend(results_tuple)
         result.append(tmp_list)
 
-    return pd.DataFrame.from_records(result, columns=['topic_id', 'weighted_slot_entropy', 'num_weighted_slots', 'weighted_pos_entropy', 'num_weighted_pos', 'weighted_lemma_entropy', 'num_weighted_lemmas', 'unweighted_slot_entropy', 'num_unweighted_slots', 'unweighted_pos_entropy', 'num_unweighted_pos', 'uweighted_lemma_entropy', 'num_unweighted_lemmas', 'weighted_ratio_slots_to_lemmas', 'weighted_ratio_pos_to_lemmas','unweighted_ratio_slots_to_lemmas', 'unweighted_ratio_pos_to_lemmas', f'lemmas_to_top_{top_n}_surface_forms', f'slots_to_top_{top_n}_surface_forms', f'pos_to_top_{top_n}_surface_forms', f'topic_coverage_by_top_{top_n}_surface_forms'])
+    return pd.DataFrame.from_records(result, columns=['topic_id', 'weighted_slot_entropy', 'num_weighted_slots', 'weighted_pos_entropy', 'num_weighted_pos', 'weighted_lemma_entropy', 'num_weighted_lemmas', 'unweighted_slot_entropy', 'num_unweighted_slots', 'unweighted_pos_entropy', 'num_unweighted_pos', 'unweighted_lemma_entropy', 'num_unweighted_lemmas', 'weighted_ratio_slots_to_lemmas', 'weighted_ratio_pos_to_lemmas','unweighted_ratio_slots_to_lemmas', 'unweighted_ratio_pos_to_lemmas', f'lemmas_to_top_{top_n}_surface_forms', f'slots_to_top_{top_n}_surface_forms', f'pos_to_top_{top_n}_surface_forms', f'topic_coverage_by_top_{top_n}_surface_forms'])
 
 
 def stem_state_file(in_state_file, out_state_file, stemmer):
